d334e30b2c0c409943def8ba887c967e44208847
yashpate521 <yash1451999@gmail.com>
2026-04-01 17:42:13 -0400

    Finalize Letter Combinations solution file

--- Medium/0017_LetterCombinationsOfAPhoneNumber.js
+++ Medium/0017_LetterCombinationsOfAPhoneNumber.js
@@ -28,34 +28,34 @@
  */
 
 const keypad = {
-    1: "",
-    2: "abc",
-    3: "def",
-    4: "ghi",
-    5: "jkl",
-    6: "mno",
-    7: "pqrs",
-    8: "tuv",
-    9: "wxyz"
-}
+  1: "",
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
+};
 var letterCombinations = function (digits) {
-    if (!digits.length) return [];
+  if (!digits.length) return [];
 
-    let result = [];
+  let result = [];
 
-    function backtrack(index, path) {
-        if (index === digits.length) {
-            result.push(path);
-            return;
-        }
-
-        let letters = keypad[digits[index]];
-
-        for (let char of letters) {
-            backtrack(index + 1, path + char);
-        }
+  function backtrack(index, path) {
+    if (index === digits.length) {
+      result.push(path);
+      return;
     }
 
-    backtrack(0, "");
-    return result;
+    let letters = keypad[digits[index]];
+
+    for (let char of letters) {
+      backtrack(index + 1, path + char);
+    }
+  }
+
+  backtrack(0, "");
+  return result;
 };

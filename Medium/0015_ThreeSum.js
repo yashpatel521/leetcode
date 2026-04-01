e92356bb03ea104aecf4b2a30ed5e8dcf849fe83
yashpate521 <yash1451999@gmail.com>
2026-04-01 11:47:38 -0400

    Finalize 3Sum solution file update

--- Medium/0015_ThreeSum.js
+++ Medium/0015_ThreeSum.js
@@ -27,32 +27,32 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-    let arr = nums.sort((a, b) => a - b);
-    let result = [];
+var threeSum = function (nums) {
+  let arr = nums.sort((a, b) => a - b);
+  let result = [];
 
-    for (let i = 0; i < arr.length; i++) {
-        if (i > 0 && arr[i] === arr[i - 1]) continue;
+  for (let i = 0; i < arr.length; i++) {
+    if (i > 0 && arr[i] === arr[i - 1]) continue;
 
-        let left = i + 1;
-        let right = arr.length - 1;
+    let left = i + 1;
+    let right = arr.length - 1;
 
-        while (left < right) {
-            let sum = arr[i] + arr[left] + arr[right];
-            if (sum == 0) {
-                result.push([arr[i], arr[left], arr[right]]);
+    while (left < right) {
+      let sum = arr[i] + arr[left] + arr[right];
+      if (sum == 0) {
+        result.push([arr[i], arr[left], arr[right]]);
 
-                while (left < right && arr[left] === arr[left + 1]) left++;
-                while (left < right && arr[right] === arr[right - 1]) right--;
+        while (left < right && arr[left] === arr[left + 1]) left++;
+        while (left < right && arr[right] === arr[right - 1]) right--;
 
-                left++;
-                right--;
-            } else if (sum < 0) {
-                left++;
-            } else {
-                right--;
-            }
-        }
+        left++;
+        right--;
+      } else if (sum < 0) {
+        left++;
+      } else {
+        right--;
+      }
     }
-    return result;
+  }
+  return result;
 };
